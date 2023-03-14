0b556760d9aaa28d6ceea3d1b1b2f22e11eef0ce
murat oz <muratozcevahir@gmail.com>
2023-03-14 23:26:12 +0300

    Test Changed

--- src/component/App.js
+++ src/component/App.js
@@ -51,6 +51,8 @@ function App() {
             <NewsContext.Provider value={newsContext}>
                 <Main />
             </NewsContext.Provider>
+
+            test değişimi
         </>
 
 
